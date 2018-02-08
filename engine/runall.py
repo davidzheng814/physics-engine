ba3928e77310dcbdc30cb8f40df90b11ba0e3add
David Zheng <dzd123@mit.edu>
2018-02-07 22:14:32 -0500

    modify to work with billf.

--- engine/runall.py
+++ engine/runall.py
@@ -2,7 +2,7 @@ import subprocess
 import os
 
 cmd1 = 'node physics.js -n '
-cmd2 = ' --num-bodies 6 --batch -o ../jsons/data -s '
+cmd2 = ' --num-bodies 6 --batch -o /data/vision/billf/object_interaction/phys/davidzheng/data/jsons_default_train/data -s '
 # cmd2 = ' --physics collision --num-bodies 3 --fix-first --batch --min-collide -o /data/vision/oliva/scenedataset/urops/scenelayout/jsons2/data -s '
 procs = []
 n = 1000000
@@ -15,3 +15,4 @@ for i in range(0, n, step):
 
 for proc in procs:
     proc.wait()
+
